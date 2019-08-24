355d1c8af6452b2b4ef640a88d64610cd81ef326
artem <artall64@gmail.com>
2019-08-24 21:08:58 +0200

    do balance polling, hardcode TON exchange rates

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Web3Provider } from './web3-provider';
-import { chainLinkAbi, chainLinkAddress } from "./chainlink";
-import { swapAddress, swapContractAbi } from "./swap-conract";
+import { chainLinkAbi, chainLinkAddress } from './chainlink';
+import { swapAddress, swapContractAbi } from './swap-conract';
+import { combineLatest, from, Observable, Subscription, timer } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 //
 // import Web3 from 'web3';
@@ -30,7 +32,7 @@ import { swapAddress, swapContractAbi } from "./swap-conract";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isLinear = true;
   selectedCurrency = 'TON';
   amount: number;
@@ -38,34 +40,59 @@ export class AppComponent implements OnInit {
 
   balance: number;
   usdBalance: string;
+  ton2eth = 0.1;
+  eth2ton = 10;
 
   private readonly chainLink;
   private readonly swap;
+
+  private subscription: Subscription;
 
   constructor(private http: HttpClient) {
     this.chainLink = new Web3Provider(chainLinkAbi, chainLinkAddress);
     this.swap = new Web3Provider(swapContractAbi, swapAddress);
   }
 
+  // TODO: move to helper
+  private polling(doRequest: () => Promise<any>, interval: number): Observable<any> {
+    return timer(0, interval).pipe(
+      switchMap(() => {
+        return from(doRequest());
+      })
+    );
+  }
+
   async ngOnInit() {
-    // web3.currentProvider.enable()
-    // window.torus.getPublicAddress('artall64@gmail.com').then( (addr) => {debugger})
-    //
 
-    //
-    // http://35.228.96.248/getBalance/{address}/?network=basechain
-    // http://35.228.96.248/getBalance/2dc356e6c07379ae86c09fadd6ba1f858ec65bab0252f4e36c05c5ff73b9806c/?network=basechain
-    //
+    const ethBalance$ = this.polling(
+      () => this.getEthBalance(),
+      3000
+    );
 
-    // this.getExchangeRate();
-    // this.getBalance();
-    this.balance = await this.getEthBalance();
-    const rate = await this.eth2usd();
-    this.usdBalance = (Number(this.balance) * Number(rate)).toFixed(2);
+    const eth2usd$ = this.polling(
+      () => this.requestEth2usd(),
+      10000
+    );
+
+    // Subscribe to balances and exchange rates
+    this.subscription = combineLatest([ethBalance$, eth2usd$]).pipe(
+      tap((x) => {
+        const [ethBalance, eth2usd] = x;
+        this.balance = Number(ethBalance);
+        const rate = Number(eth2usd);
+        this.usdBalance = (this.balance * rate).toFixed(2);
+        // this.eth2ton = rate * 100;
+        // this.ton2eth = 1 / this.eth2ton;
+      })
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   // get eth2usd exchange rate from chainlink
-  async eth2usd(): Promise<string> {
+  async requestEth2usd(): Promise<string> {
     return await this.chainLink.callSmartContract('current');
   }
 
